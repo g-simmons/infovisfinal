f421d7131bdf1b46794f86ce501b6559de770a05
Suhayb <>
2019-03-05 21:37:51 -0800

    Multi-Filters

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,6 +1,24 @@
 function Filter() {
     var _filters = {};
 
+    function RangeFilter(range) {
+        this.values = range;
+        this.isFiltered = function (value) {
+            return (value < this.values[0] || value > this.values[1])
+        }
+    }
+
+    function CollectionFilter(values) {
+        this.values = values;
+        this.isFiltered = function (value) {
+            return this.values.includes(value);
+        }
+    }
+
+    function filterSelector(values, isRange = true) {
+        return isRange ? new RangeFilter(values) : new CollectionFilter(values);
+    }
+
     // Key is optional
     // If Key == null will clear all filters
     this.clear = function(key) {
@@ -11,24 +29,36 @@ function Filter() {
         }
     }
 
+    // Add multiple filters
+    this.add = function(key, values, isRange = true) {
+        if (!_filters[key]) {
+            this.set(key, values, isRange);
+        } else {
+            _filters[key].push(filterSelector(values, isRange));
+        }
+    }
+
     // Range is an array:
-    // if values are scalars: [minVal, maxVal]
-    // TODO: implement categorical filtering
-    this.add = function(key, range) {
-        _filters[key] = range;
+    // if isRange == true: [minVal, maxVal]
+    // else [val1, val2, val3, ...]
+    this.set = function (key, values, isRange = true) {
+        _filters[key] = [filterSelector(values, isRange)];
     }
 
     // Returns true if the value is filtered on the key
     this.isFilteredKV = function(key, value) {
-        var filter = _filters[key];
-        return filter && (value < filter[0] || value > filter[1]);
+        for (var filter in _filters[key]) {
+            if (filter.isFiltered(value)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     // Returns true if the data point is filtered for any of it's keys
     this.isFiltered = function(dataPoint) {
         for (var key in _filters) {
-            var value = dataPoint[key];
-            if (this.isFilteredKeyValue(key, value)) {
+            if (this.isFilteredKeyValue(key, dataPoint[key])) {
                 return true;
             }
         }
