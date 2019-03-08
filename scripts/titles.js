8d5cb696454f5ae094741df03a2260cba006ce0e
Suhayb <38865274+Suhayb-A@users.noreply.github.com>
2019-03-07 16:22:46 -0800

    Titles

---
+++ scripts/titles.js
@@ -0,0 +1,21 @@
+// Formatted display title for dimension
+// "dim": "Full dimension title",
+var _titles = {
+
+};
+
+
+// Formatted display title for catagories
+// "categoryName": ["Group 1", "Group 2"],
+var _categoryTitles = {
+
+}
+
+// Returns formatted titles for dimention
+// If the values are catagorical then adding the value will return the value's title
+function title(dimension, value = null) {
+    if (value) {
+        return _categoryTitles[key][value] || value;
+    }
+    return _titles[dimension] || dimension;
+}
