c7e3248b7578de4eeb19b223707aabe0f87ee480
Suhayb <>
2019-03-10 13:02:57 -0700

    minor

--- scripts/index.js
+++ scripts/index.js
@@ -25,11 +25,17 @@ d3.csv("./data/foods_final.csv", function (error, rawData) {
     data = rawData;
 
     data.forEach(function(d) {
+        
+        d3.keys(d).forEach(key => {
+            //TODO: Manually convert for accuracy!
+            //Source: stackoverflow.com/questions/175739/
+            d[key] = (isNaN(+d[key]) ? d[key] : Number(d[key]));
+        });
+
         d.ax1 = +d.ax1;
         d.ax2 = +d.ax2;
-
     });
-    console.log(data);
+    // console.log(data);
 
     parallelC = new ParallelCoordinates(d3.select(".parallelC"), parallelCDimentions);
     scatterplot = new ScatterPlot(d3.select(".scatterplot"));
