3c9251506cc214b2d35ffff5574a51dde4179a4d
Suhayb <>
2019-03-10 11:50:35 -0700

    Parallel-C: Titles & Bug fixes

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,11 @@ var data;
 var groupKey = "";
 
 var parallelC;
+//The dimentions that would be shown on the parallel coordinates
+var parallelCDimentions = [
+    "Organoheterocyclic compounds",
+    "Organooxygen compounds"
+]
 
 function filterChanged(key = null) {
     filter.mark();
@@ -15,8 +20,15 @@ var color = new Color(d3.schemeCategory10);
 // Process the data
 d3.csv("./data/foods_final.csv", function (error, rawData) {
 
-    data = rawData;
+    data = rawData.map(d => {        
+        d3.keys(d).forEach(key => {
+            //TODO: Manually convert for accuracy!
+            //Source: stackoverflow.com/questions/175739/
+            d[key] = (isNaN(+d[key]) ? d[key] : Number(d[key]));
+        });
+        return d;
+    });
     console.log(data);
 
-    parallelC = new ParallelCoordinates(d3.select(".parallelC"), []);
+    parallelC = new ParallelCoordinates(d3.select(".parallelC"), parallelCDimentions);
 });
